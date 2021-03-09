1f7a9aed87a5b6d62540a56d875f27d1156ecd8c
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2021-03-09 11:03:18 -0400

    first Step and conection Mongo

---
+++ backend/db/mongodb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.connection.on('open', () => {
+  console.log('db conencted')
+})
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  };
+
+const connectDb = async ({ host, port, dbName }) => {
+    const uri = `mongodb://${host}:${port}/${dbName}`
+    mongoose.connect(uri, options)
+}
+
+module.exports = connectDb
